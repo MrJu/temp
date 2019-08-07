d12e6526843a159314d4a317e861ac2f295d0f0b
andrew <mrju.email@gmail.com>
2019-08-07 12:33:16 +0800

    add basic platform driver 2nd
    Signed-off-by: andrew <mrju.email@gmail.com>

---
+++ basic_platform_driver_2nd/foo_drv.c
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2019 Andrew <mrju.email@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ */
+
+#include <linux/module.h>
+#include <linux/platform_device.h>
+
+#define STR(x) _STR(x)
+#define _STR(x) #x
+
+#define VERSION_PREFIX Foo
+#define MAJOR_VERSION 1
+#define MINOR_VERSION 0
+#define PATCH_VERSION 0
+
+#define VERSION STR(VERSION_PREFIX-MAJOR_VERSION.MINOR_VERSION.PATCH_VERSION)
+
+#define DEVICE_NAME "foo"
+
+static int foo_probe(struct platform_device *pdev)
+{
+	printk("%s(): %d\n", __func__, __LINE__);
+	return 0;
+}
+
+static int foo_remove(struct platform_device *pdev)
+{
+	printk("%s(): %d\n", __func__, __LINE__);
+	return 0;
+}
+
+static struct platform_driver foo_drv = {
+	.probe	= foo_probe,
+	.remove	= foo_remove,
+	.driver	= {
+		.name = DEVICE_NAME,
+	}
+};
+
+static int __init foo_init(void)
+{
+	int ret;
+	struct platform_device *pdev;
+
+	pdev = platform_device_register_simple(DEVICE_NAME, -1, NULL, 0);
+	if (IS_ERR(pdev))
+		return PTR_ERR(pdev);
+
+	ret = platform_driver_register(&foo_drv);
+	if (ret) {
+		platform_device_unregister(pdev);
+		return ret;
+	}
+
+	return 0;
+}
+
+static void __exit foo_exit(void)
+{
+	struct device *dev;
+    
+	dev = bus_find_device_by_name(foo_drv.driver.bus, NULL, DEVICE_NAME);
+	if (dev)
+		platform_device_unregister(to_platform_device(dev));
+
+	platform_driver_unregister(&foo_drv);
+}
+
+module_init(foo_init);
+module_exit(foo_exit);
+
+MODULE_ALIAS("foo-driver");
+MODULE_LICENSE("GPL");
+MODULE_VERSION(VERSION);
+MODULE_DESCRIPTION("Linux is not Unix");
+MODULE_AUTHOR("andrew, mrju.email@gmail.com");
